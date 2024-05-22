39fe348c2898019a4a0ea7ae05253bb403d9d285
havi moon <blue@havis-MacBook-Pro.local>
2024-05-22 18:47:15 +0900

    edit s3 name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # tfstate를 저장할 backend 설정
 terraform {
   backend "s3" {
-    bucket = "moonblue-tfstate1"
+    bucket = "moonblue-tfstate2"
     key    = "terraform.state"
     region = "us-east-1"
     # profile = "fastc"
