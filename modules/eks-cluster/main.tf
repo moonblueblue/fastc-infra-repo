25b50e83bc0d414a7f32c6833846fc28e991a81a
havi moon <blue@havis-MacBook-Pro.local>
2024-05-22 15:21:45 +0900

    changed aws iam account id

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -72,14 +72,14 @@ module "eks" {
 
   aws_auth_users = [
     {
-      userarn  = "arn:aws:iam::672261773768:user/admin"
+      userarn  = "arn:aws:iam::kk_labs_user_418240:user/admin"
       username = "admin"
       groups   = ["system:masters"]
     },
   ]
 
   aws_auth_accounts = [
-    "672261773768"
+    "kk_labs_user_418240"
   ]
 }
 
